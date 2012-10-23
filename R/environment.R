904e40dedf2dc8082e5c23bc2fcb8dbce00e8a6a
Laurent <laurent.gatto@gmail.com>
2012-10-24 00:05:04 +0100

    fixed vignette, added data, documentation

--- R/environment.R
+++ R/environment.R
@@ -8,19 +8,51 @@ stockcol <-  c(brewer.pal(9, "Set1"),
                "#00008A", ## blue4
                "#FF6347") ## tomato
 assign("stockcol", stockcol, envir = .pRolocEnv)
-getStockcol <- function() get("stockcol", envir=.pRolocEnv)
 
 stockpch <- c(15, 17,19, 18, 23:25, 7, 9, 13, 3:4,  8)
 assign("stockpch", stockpch, envir = .pRolocEnv)
-getStockpch <- function() get("stockpch", envir=.pRolocEnv)
-
 
 unknowncol <- "#E7E7E7" ## grey91
 assign("unknowncol", unknowncol, envir = .pRolocEnv)
-getUnknowncol <- function() get("unknowncol", envir=.pRolocEnv)
-
 
 unknownpch <- 21
 assign("unknownpch", unknownpch, envir = .pRolocEnv)
-getUnknownpch <- function() get("unknownpch", envir=.pRolocEnv)
 
+
+
+##' Returns the default cluster colours.
+##'
+##' @title Default cluster colours
+##' @return A \code{character} vector.
+##' @author Laurent Gatto
+##' @rdname getStockcol
+##' @examples
+##' getStockcol()
+getStockcol <- function() get("stockcol", envir=.pRolocEnv)
+
+##' Returns the default cluster point symbols.
+##'
+##' @title Default cluster point symbols
+##' @return A \code{numeric} vector.
+##' @rdname getStockcol
+##' @examples
+##' getStockpch()
+getStockpch <- function() get("stockpch", envir=.pRolocEnv)
+
+##' Returns the default colour for unknown features.
+##'
+##' @title Default colour for unknown features
+##' @return A \code{character} vector or length 1.
+##' @rdname getStockcol
+##' @examples
+##' getUnknowncol()
+getUnknowncol <- function() get("unknowncol", envir=.pRolocEnv)
+
+##' Returns the default point character for unknown features.
+##'
+##' @title Default point character for unknown features
+##' @return A \code{numeric} vector of length 1.
+##' @rdname getStockcol
+##' @examples
+##' getUnknownpch()
+getUnknownpch <- function() get("unknownpch", envir=.pRolocEnv)

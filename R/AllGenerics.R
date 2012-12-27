1f2dea3e5b453302fb2867af6402bff63ae2f0d3
Laurent Gatto <l.gatto@dnavision.be>
2012-12-27 04:53:54 +0000

    Merge branch 'master' (v0.99.5) into local-hedgehog (V0.99.4)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@72180 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,13 +1,11 @@
-## MSnSet
-setGeneric("exprsToRatios",function(object,...) standardGeneric("exprsToRatios"))
-
 ## machinelearning-framework
 setGeneric("getWarnings", function(object) standardGeneric("getWarnings"))
 setGeneric("getSeed", function(object) standardGeneric("getSeed"))
 setGeneric("getF1Scores", function(object) standardGeneric("getF1Scores"))
-setGeneric("getRegularisedParams", function(object) standardGeneric("getRegularisedParams"))
-setGeneric("getRegularizedParams", function(object) standardGeneric("getRegularizedParams"))
 setGeneric("levelPlot", function(object, ...) standardGeneric("levelPlot"))
+setGeneric("getParams", function(object) standardGeneric("getParams"))
+setGeneric("getRegularisedParams", function(object) getParams(object))
+setGeneric("getRegularizedParams", function(object) getParams(object))
 
 setGeneric("chi2",function(x,y,...) standardGeneric("chi2"))
 

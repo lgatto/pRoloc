9cb2f70dcb0badbe1d0bc20cde70847e7d120403
Laurent <laurent.gatto@gmail.com>
2013-03-26 09:07:36 +0000

    lopims pipeline

--- R/lopims.R
+++ R/lopims.R
@@ -27,7 +27,7 @@
   ## iterate of each file pairs
   res <- lapply(seq_along(msefiles),
                 function(i) {
-                  message("[LOPIMS 2] Synergising files ", i, " out of ", n)
+                  message("[LOPIMS 2] Synergising file ", i, " out of ", n)
                   .outputdir <- file.path(outdir, paste0("synapter_results_", i))
                   dir.create(.outputdir)
                   .input <- list(identpeptide = masterfile,
@@ -54,30 +54,49 @@
   Reduce(combine, ll)
 }
 
+refNormMedOfPepRatio <- function(x) {
+  ## Polpitiya, A., Qian, W., Jaitly, N., Petyuk, V. et al., 
+  ## DAnTE: a statistical tool for quantitative analysis of -omics data. 
+  ## Bioinformatics 2008, 24, 1556–1558.
+  ##
+  ## See also Matzke et al. Proteomics DOI 10.1002/pmic.201200269
+  ## Ratio all peptides to the peptide with the least amount of missing values;
+  ## Protein abundance is the median of the scaled peptide abundances
+  ref <- which.min(apply(x, 2, function(.x) sum(is.na(.x))))
+  x <- as.matrix(x)
+  .res <- apply(x, 1, function(.x) {
+    .xref <- .x[ref]
+    .x/.xref
+  })
+  res <- t(.res)
+  apply(res, 2, median, na.rm = TRUE)
+}  
+
+refNormMeanOfNonNAPepSum <- function(x) {
+  ## For each protein:
+  ## (1) Find ref fraction with least amount of missing values
+  ## (2) For each (frac_i, frac_ref), consider only peptides
+  ##     without missing values and
+  ##     calculate mean(sum(frac_i), sum(frac_ref))
+  ref <- which.min(apply(x, 2, function(.x) sum(is.na(.x))))
+  x <- as.matrix(x)
+  .res <- rep(-1, ncol(x))
+  for (i in 1:ncol(x)) {
+    sel_i <- !is.na(x[, i])
+    sel_ref <- !is.na(x[, ref])
+    sel <- sel_i & sel_ref
+    .res[i] <- sum(x[sel, i])/sum(x[sel, ref])
+  }
+  .res
+}  
+
 .lopims4 <- function(xx) {
   ## remove rows with only NAs
   xx <- xx[!apply(exprs(xx), 1, function(x) all(is.na(x))), ]
   message("[LOPIMS 4] Calculate ratios to reference peptide")
-  refNorm <- function(x) {
-    ## Polpitiya, A., Qian, W., Jaitly, N., Petyuk, V. et al., 
-    ## DAnTE: a statistical tool for quantitative analysis of -omics data. 
-    ## Bioinformatics 2008, 24, 1556–1558.
-    ##
-    ## See also Matzke et al. Proteomics DOI 10.1002/pmic.201200269
-    ## Ratio all peptides to the peptide with the least amount of missing values;
-    ## Protein abundance is the median of the scaled peptide abundances
-    ref <- which.min(apply(x, 2, function(.x) sum(is.na(.x))))
-    x <- as.matrix(x)
-    .res <- apply(x, 1, function(.x) {
-      .xref <- .x[ref]
-      .x/.xref
-    })
-    res <- t(.res)
-    apply(res, 2, median, na.rm = TRUE)
-  }  
   res <- by(exprs(xx),
             fData(xx)$protein.Accession.LOPIMS1,
-            refNorm)  
+            refNormMeanOfNonNAPepSum)  
   res2 <- Reduce(rbind, res)
   rownames(res2) <- names(res)
   xx2 <- combineFeatures(xx, fData(xx)$protein.Accession.LOPIMS1, fun = median)
@@ -85,11 +104,14 @@
   exprs(xx2) <- res2
   xx2@processingData@processing <-
     c(xx2@processingData@processing,
-      paste0("Normalised intensities to reference peptide: ",
+      paste0("Normalised intensities to reference fraction: ",
              date()))
+  sampleNames(xx2) <- sampleNames(xx)
   if (validObject(xx2))
     return(xx2)
 }
+
+
 
 .lopims5 <- function(x, markerfile) {
   message("[LOPIMS 5] Adding markers")
@@ -103,6 +125,7 @@ lopims <- function(hdmsedir = "HDMSE",
                    markerfile,
                    mfdr = 0.025,
                    ...) {
+
   suppressPackageStartupMessages(library("MSnbase"))
   suppressPackageStartupMessages(library("synapter"))
   suppressPackageStartupMessages(library("pRoloc"))

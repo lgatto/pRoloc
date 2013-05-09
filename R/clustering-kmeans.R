2dacb9839e374e89cfdf7d29886d3d32dd034d07
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-05-09 15:21:57 +0000

    kmeans BIC/AIC
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@76346 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/clustering-kmeans.R
+++ R/clustering-kmeans.R
@@ -1,3 +1,44 @@
+.optim <- function(k, object, cl,
+                   criterion = c("BIC", "AIC"), ...) {
+  
+  criterion <- match.arg(criterion, several.ok = TRUE) 
+  .var <- function(x) 
+    sum(x^2 - mean(x)^2)/length(x)
+  
+  X <- exprs(object)
+  N <- nrow(X)
+  P <- ncol(X)
+  if (missing(cl))
+    cl <- kmeans(X, centers = k, ...)
+  Y <- cl$cluster
+  Nc <- cl$size
+  K <- length(Nc)
+
+  ## per cluster variance - using N instead of N-1 for clusters of size 1
+  Vc <- matrix(0, nrow = P, ncol = K)
+  rownames(Vc) <- colnames(X)
+  colnames(Vc) <- 1:K
+  for (k in 1:K) {
+    for (p in 1:P) {
+      Vc[p, k] <- .var(X[Y == k, p]) 
+    }
+  }
+  ## total variance
+  V <- matrix(apply(X, 2, var), ncol = K, nrow = P)
+  LL <- -Nc * colSums(log(Vc + V)/2) 
+  .BIC <- -2 * sum(LL) + 2 * K * P * log(N)
+  .AIC <- -2 * sum(LL) + 4 * K * P
+  if (length(criterion) == 2) {
+    ans <- list(BIC = .BIC, AIC = .AIC)
+  } else {
+    ans <- switch(criterion,
+                  BIC = .BIC,
+                  AIC = .AIC)
+  }
+  return(ans)
+}
+
+
 setGeneric("kmeansClustering",
            function(object, params, ...)
            standardGeneric("kmeansClustering"))
@@ -31,6 +72,6 @@ setMethod("kmeansClustering",
 
 setMethod("kmeansOptimisation",
           signature(object = "MSnSet"),
-          function(object, ...) {
-            ## optimise and create/return a ClustGenRes
+          function(object, k = 1:20, ...) {
+            sapply(1:20, .optim, object, ...)
           })

a02d960583d0d40b0805b45210c50110c975755d
Laurent <laurent.gatto@gmail.com>
2013-07-01 19:35:08 +0100

    default args in nndist

--- R/distances.R
+++ R/distances.R
@@ -1,21 +1,23 @@
-setMethod("nndist", "matrix", function(object, ...) nndist_matrix(object))
-setMethod("nndist", "MSnSet", function(object, ...) nndist_msnset(object))
+setMethod("nndist", "matrix",
+          function(object, k = 3, dist = c("euclidian", "mahalanobis"), ...)
+          nndist_matrix(object, k, match.arg(dist), ...))
+setMethod("nndist", "MSnSet",
+          function(object, k = 3, dist = c("euclidian", "mahalanobis"), ...)
+          nndist_msnset(object, k, match.arg(dist), ...))
 
 .mahalanobis <- function(X, y, S = cov(X)) mahalanobis(X, y, S)
 
-nndist_matrix <- function(object, k = 3,
-                   dist = c("euclidian", "mahalanobis"),
-                   ...) {
-    dist <- match.arg(dist) ## supported distances: euclidian, mahalanobis
+nndist_matrix <- function(object, k, dist = dist, ...) {
     m <- seq(1, k * 2, 2)
     ans <- matrix(NA, nrow = nrow(object), ncol = k * 2)
     rownames(ans) <- rownames(object)
     colnames(ans) <- paste0(rep(c("index", "dist"), k),
-                            rep(1:k, each = 2))    
+                            rep(1:k, each = 2)) 
     if (dist == "euclidian") {
         res <- get.knn(object, k = k, ...) # n by k matrix
         ans[, m] <- res$nn.index
         ans[, m + 1] <- res$nn.dist
+        colnames(ans) <- paste0(colnames(ans), "euc")
     } else { ## mahalanobis   
         S <- cov(object)
         res <- apply(object, 1, ## n by n distance matrix
@@ -23,20 +25,17 @@ nndist_matrix <- function(object, k = 3,
         diag(res) <- Inf 
         idx <- apply(res, 2, order)
         ans[, m] <- t(idx[1:k, ])
-        ans[, m + 1] <- matrix(res[idx[1:k, ]], nrow = nrow(object), ncol = k)                
+        ans[, m + 1] <- matrix(res[idx[1:k, ]], nrow = nrow(object), ncol = k)
+        colnames(ans) <- paste0(colnames(ans), "mah")
     }
     return(ans)
 }
 
-nndist_msnset <- function(object, k = 3,
-                          dist = "euclidian" ,
-                          ...) {
-    res <- nndist(exprs(object), k = k,
-                  dist = dist, ...)
+nndist_msnset <- function(object, k, dist = dist, ...) {
+    res <- nndist_matrix(exprs(object), k, dist, ...)                  
     fData(object) <- cbind(fData(object),
                            res)
     if (validObject(object))
         return(object)
 }
-
 

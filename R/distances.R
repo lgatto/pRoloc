9c2c82ccf7c4309e50acc9a574dacea0f5a75c5a
Laurent <laurent.gatto@gmail.com>
2013-07-01 16:39:10 +0100

    mahalanobis nndist

--- R/distances.R
+++ R/distances.R
@@ -1,20 +1,31 @@
 setMethod("nndist", "matrix", function(object, ...) nndist_matrix(object))
 setMethod("nndist", "MSnSet", function(object, ...) nndist_msnset(object))
 
+.mahalanobis <- function(X, y, S = cov(X)) mahalanobis(X, y, S)
+
 nndist_matrix <- function(object, k = 3,
-                   dist = "euclidian" ,
+                   dist = c("euclidian", "mahalanobis"),
                    ...) {
-    dist <- match.arg(dist)
-    ## supported distances: euclidian
-    ## to do: mahalanobis, possibly other from dist
-    res <- get.knn(object, k = k, ...)
+    dist <- match.arg(dist) ## supported distances: euclidian, mahalanobis
     m <- seq(1, k * 2, 2)
     ans <- matrix(NA, nrow = nrow(object), ncol = k * 2)
-    ans[, m] <- res$nn.index
-    ans[, m + 1] <- res$nn.dist
     rownames(ans) <- rownames(object)
     colnames(ans) <- paste0(rep(c("index", "dist"), k),
-                            rep(1:k, each = 2))
+                            rep(1:k, each = 2))    
+    if (dist == "euclidian") {
+        res <- get.knn(object, k = k, ...) # n by k matrix
+        ans[, m] <- res$nn.index
+        ans[, m + 1] <- res$nn.dist
+    } else { ## mahalanobis   
+
+        S <- cov(object)
+        res <- apply(object, 1, ## n by n distance matrix
+                     function(x) .mahalanobis(object, x, S))
+        diag(res) <- Inf 
+        idx <- apply(res, 2, order)
+        ans[, m] <- t(idx[1:k, ])
+        ans[, m + 1] <- matrix(res[idx[1:k, ]], nrow = nrow(object), ncol = k)                
+    }
     return(ans)
 }
 
@@ -29,4 +40,4 @@ nndist_msnset <- function(object, k = 3,
         return(object)
 }
 
-    
+

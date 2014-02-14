0e77bdd65a428d9ba28f6bca34501a3ee65f1ed6
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-14 20:07:11 +0000

    pd testing script
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@86490 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/test_pd.R
+++ inst/scripts/test_pd.R
@@ -5,9 +5,18 @@ data(dunkley2006)
 ## Test original PD
 ## dunkley2006pdunittest1 <- phenoDisco(dunkley2006, times=100, allIter=TRUE, 
 ##                                     seed=1, GS=10, verbose=TRUE)
-## save(dunkley2006pdunittest1, file="dunkley2006pdunittest1.rda")
+## save(dunkley2006pdunittest1, file="../extdata/dunkley2006pdunittest1.rda")
 
 ## Test new PD
 ## dunkley2006pdunittest2 <- phenoDisco2(dunkley2006, times=100, allIter=TRUE, 
 ##                                     seed=1, GS=10, verbose=TRUE, BPPARAM=NULL)
-## save(dunkley2006pdunittest2, file="dunkley2006pdunittest2.rda")
+## save(dunkley2006pdunittest2, file="../extdata/dunkley2006pdunittest2.rda")
+
+## Testing PD updates
+dunkley2006pdunittest <- phenoDisco(dunkley2006, times=100, allIter=TRUE, 
+                                    seed=1, GS=10, verbose=TRUE, BPPARAM=NULL)
+load("../extdata/dunkley2006pdunittest2.rda")
+all.equal(dunkley2006pdunittest, dunkley2006pdunittest2)
+stopifnot(all.equal(fData(dunkley2006pdunittest), fData(dunkley2006pdunittest2)))
+
+

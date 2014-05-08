f86e3a1a79ec10a7f183b12d5bcd30788e83c800
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-08 02:16:32 +0000

    Merge branch 'master' into local-hedgehog
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@90063 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/markers.R
+++ R/markers.R
@@ -2,20 +2,27 @@
 ## data is in inst/extdata/marker_species.rda
 
 
-##' This function retrieves a list of organelle markers or,
-##' if not \code{species} is provided, prints a description
-##' of available markers sets. The markers can be added to
-##' and \code{MSnSet} using the \code{\link{addMarkers}}
-##' function.
+##' This function retrieves a list of organelle markers or, if no
+##' \code{species} is provided, prints a description of available
+##' marker sets. The markers can be added to and \code{MSnSet} using
+##' the \code{\link{addMarkers}} function.
 ##'
 ##' The markers have been contributed by various members of the
-##' Cambridge Centre for proteomics, in particular Dan Nightingale
-##' for yeast, Dr Andy Christoforou for human, Dr Arnoud Groen for
-##' Arabodopsis and Dr Claire Mulvey for mouse.
-##' In addition, markers from the \code{pRolocdata} datasets
-##' have been extracted. See \code{pRolocdata} for details and
-##' references.
+##' Cambridge Centre for Proteomics, in particular Dan Nightingale for
+##' yeast, Dr Andy Christoforou for human, Dr Arnoud Groen for
+##' Arabodopsis and Dr Claire Mulvey for mouse.  In addition, original
+##' (curated) markers from the \code{pRolocdata} datasets have been
+##' extracted (see \code{pRolocdata} for details and references).
+##' Curation involved verification of publicly available subcellular
+##' localisation annotation based on the curators knowledge of the
+##' organelles/proteins considered and tracing the original statement
+##' in the literature.
 ##'
+##' These markers are provided as a starting point to generate
+##' reliable sets of organelle markers but still need to be verified
+##' against any new data in the light of the quantitative data and the
+##' study conditions.
+##' 
 ##' @title Organelle markers
 ##' @param species The species of interest.
 ##' @return Prints a description of the available marker lists if

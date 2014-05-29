8b16abe4d8da5c58a20b4597367d710a2d389132
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-29 09:00:51 +0000

    fixed selection
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@90825 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/test_data.R
+++ inst/tests/test_data.R
@@ -4,6 +4,6 @@ data(dunkley2006)
 
 test_that("subsetting", {
   mrk <- markerSet(dunkley2006)
-  nm <- sum(fData(dunkley2006)$markers == "unknown")  
+  nm <- sum(fData(dunkley2006)$markers == "unknown")
   expect_equal(nrow(dunkley2006) - nm, nrow(mrk))
 }

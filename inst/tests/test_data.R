2207488d30e600b2e3fad0bd4a34686b3ede866e
Laurent <laurent.gatto@gmail.com>
2014-05-29 23:43:01 +0100

    cleaning

--- inst/tests/test_data.R
+++
@@ -1,9 +0,0 @@
-context("Data consistence")
-
-data(dunkley2006)
-
-test_that("subsetting", {
-  mrk <- markerSet(dunkley2006)
-  nm <- sum(fData(dunkley2006)$markers == "unknown")
-  expect_equal(nrow(dunkley2006) - nm, nrow(mrk))
-})

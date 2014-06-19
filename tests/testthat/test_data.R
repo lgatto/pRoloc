d1e2142bc63d68d2207ba6b7720baa97b880e6ba
Laurent <lg390@cam.ac.uk>
2014-06-19 17:00:05 +0100

    update rename in test

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -3,7 +3,7 @@ context("Data consistence")
 data(dunkley2006)
 
 test_that("subsetting", {
-  mrk <- markerSet(dunkley2006)
+  mrk <- markerMSnSet(dunkley2006)
   nm <- sum(fData(dunkley2006)$markers == "unknown")
   expect_equal(nrow(dunkley2006) - nm, nrow(mrk))
 })

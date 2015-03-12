4c932fab0feaf511900c4df5fe1be4be8563441f
Laurent <lg390@cam.ac.uk>
2015-03-12 17:24:31 +0000

    deprecate getRegularisedParams

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -4,9 +4,21 @@ setGeneric("getSeed", function(object) standardGeneric("getSeed"))
 setGeneric("getF1Scores", function(object) standardGeneric("getF1Scores"))
 setGeneric("levelPlot", function(object, ...) standardGeneric("levelPlot"))
 setGeneric("getParams", function(object, ...) standardGeneric("getParams"))
+
+setGeneric("getRegularisedParams",
+           function(object) {
+               .Deprecated("getParams", "pRoloc")
+               getParams(object)
+           })
+
+setGeneric("getRegularizedParams",
+           function(object) {
+               .Deprecated("getParams", "pRoloc")
+               getParams(object)
+           })
+
 setGeneric("getOtherParams", function(object, ...) standardGeneric("getOtherParams"))
-setGeneric("getRegularisedParams", function(object) getParams(object))
-setGeneric("getRegularizedParams", function(object) getParams(object))
+
 setGeneric("f1Count", function(object, ...) standardGeneric("f1Count"))
 
 setGeneric("chi2", function(x,y,...) standardGeneric("chi2"))

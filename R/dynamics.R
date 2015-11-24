4ccae3011d7ef56ac0c58978c8e475b59e5dfe6f
Laurent <lg390@cam.ac.uk>
2015-11-24 14:52:10 +0000

    adding pdist code

--- R/dynamics.R
+++ R/dynamics.R
@@ -20,3 +20,88 @@ remap <- function(object, ref = 1) {
     if (validObject(object))
         return(object)
 }
+
+
+
+pdist <- function(object, pcol, distfun = dist) {
+    if (is.character(pcol)) {
+        stopifnot(pcol %in% names(pData(object)))
+        pcol <- pData(object)[, pcol]
+    }
+    pcol <- factor(pcol)
+    stopifnot(length(levels(pcol)) == 2)
+    stopifnot(length(pcol) == ncol(object))
+    lv <- levels(pcol)
+    e1 <- exprs(object)[, pcol == lv[1]]
+    e2 <- exprs(object)[, pcol == lv[2]]
+    ans <- sapply(seq_len(nrow(object)),
+                  function(i) distfun(rbind(e1[i, ], e2[i, ])))
+    names(ans) <- featureNames(object)
+    ans
+}
+
+.sameNbCol <- function(x)
+    length(unique(sapply(msnsets(x), ncol)) == 1)
+
+.sameNbRow <- function(x)
+    length(unique(sapply(msnsets(x), nrow)) == 1)
+
+
+.pdist <- function(x, y) {
+    e1 <- exprs(x)
+    e2 <- exprs(y)
+    sapply(seq_len(nrow(object)),
+           function(i) distfun(rbind(e1[i, ], e2[i, ])))
+}
+
+pdist <- function(object, distfun = dist) {
+    stopifnot(inherits(object, "MSnSetList"))
+    ## keeping common feature names
+    object <- commonFeatureNames(object)
+    if (!.sameNbCol(object))
+        stop("All MSnSets must have the same number of columns.")
+        ## naming, to name the results at the end
+    if (is.null(names(object)))
+        names(object) <- seq_len(length(object))
+    pw <- combn(length(object), 2)
+    ans <- matrix(NA, ncol = ncol(pw),
+                  nrow = nrow(object[[1]]))
+
+    apply(pw, 2, function(ii) names(object))
+    
+    for (k in 1:ncol(pw)) {
+        i <- pw[1, k]
+        j <- pw[2, k]
+        ans[, k]. <- pdist(object[[i]], object[[j]])
+    }
+    
+
+}
+
+ 
+## d <- pdist(yeastgg2, "condition")
+## n <- 5
+## o <- order(d, decreasing = TRUE)
+## d[o[1:n]]
+
+## yggfoi <- FeaturesOfInterest(description = "top dists", fnames = names(d[o])[1:n])
+
+## p <- plot2D(yeastgg2[, yeastgg2$condition == "Glu"])
+## highlightOnPlot(yeastgg2[, yeastgg2$condition == "Glu"],
+##                 yggfoi, pch = 19, cex = 2)
+## text(p[foi(yggfoi), ], labels = 1:n, col = "white")
+
+
+## p <- plot2D(yeastgg2[, yeastgg2$condition == "Gly"])
+## highlightOnPlot(yeastgg2[, yeastgg2$condition == "Gly"],
+##                 yggfoi, pch = 19, cex = 2)
+## text(p[foi(yggfoi), ], labels = 1:n, col = "white")
+
+
+## yyg1 <- yeastgg2[, yeastgg2$replicate == 1]
+## yyg2 <- yeastgg2[, yeastgg2$replicate == 2]
+## d1 <- pdist(yyg1, "condition")
+## d2 <- pdist(yyg2, "condition")
+
+## plot((d1+d2)/2, log2(d1/d2))
+## abline(h = 0)

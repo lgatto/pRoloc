d9afa72b5a4a97351562ada46e760568021b6b8f
Laurent <lg390@cam.ac.uk>
2015-12-26 00:48:51 +0000

    load data in unit test

--- tests/testthat/test_classWeights.R
+++ tests/testthat/test_classWeights.R
@@ -1,8 +1,11 @@
 context("Calculate class weights")
 
-data(dunkley2006)
 
 test_that("classWeights", {
+    library("pRolocdata")
+    data(dunkley2006)
+    data(tan2009r1)
+
     w <- 1/c(14, 45, 28, 55, 20, 46, 19, 13, 21)
     expect_true(all(classWeights(dunkley2006) == w))
     
@@ -11,3 +14,4 @@ test_that("classWeights", {
     (w <- 1/w[names(w) != "unknown"])
     expect_identical(classWeights(tan2009r1, "pd.markers"), w)
 })
+

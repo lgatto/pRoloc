829759718a561077dd6e3cfc9670b505592d10f2
Laurent <lg390@cam.ac.uk>
2015-12-26 00:22:41 +0000

    fix syntax error in unit test

--- tests/testthat/test_classWeights.R
+++ tests/testthat/test_classWeights.R
@@ -2,7 +2,7 @@ context("Calculate class weights")
 
 data(dunkley2006)
 
-test_that("classWeights" {
+test_that("classWeights", {
     w <- 1/c(14, 45, 28, 55, 20, 46, 19, 13, 21)
     expect_true(all(classWeights(dunkley2006) == w))
     

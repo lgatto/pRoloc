562219f1d3d112549616a8cae0046b7c19908633
Laurent <lg390@cam.ac.uk>
2016-01-18 11:09:26 +0000

    fix var name typo

--- inst/scripts/dynamics.R
+++ inst/scripts/dynamics.R
@@ -4,7 +4,7 @@ remap <- function(object, ref = 1) {
     x <- msnsets(object)
     refset <- x[[ref]]
     pca1 <- prcomp(exprs(refset), scale = TRUE, center = TRUE)
-    colnames(pca1$x) <- paste0("PC", 1:ncol(refest))
+    colnames(pca1$x) <- paste0("PC", 1:ncol(reset))
     preds <- lapply(x, function(xx) {
         ans <- predict(pca1, newdata = exprs(xx))
         colnames(ans) <- paste0("PC", 1:ncol(xx))

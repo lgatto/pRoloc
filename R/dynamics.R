b969dd8f63f469303b784b0f1eab2b692f51f6b0
Laurent Gatto <l.gatto@dnavision.be>
2016-01-19 15:59:10 +0000

    Merge branch 'master' into devel
    * master:
      update news
      update biorxiv doi
      add new file to collate
      mv dynamics functions to R/dynamics.R
      remove included mart script
      update sample names to PCi
      fix var name typo
      fix var name typo
      remap names columns PCi
      dynamics code in scripts
      new github devel version
    
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@112692 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/dynamics.R
@@ -0,0 +1,56 @@
+
+remap <- function(object, ref = 1) {
+    stopifnot(inherits(object, "MSnSetList"))
+    x <- msnsets(object)
+    refset <- x[[ref]]
+    pca1 <- prcomp(exprs(refset), scale = TRUE, center = TRUE)
+    preds <- lapply(x, function(xx) {
+        ans <- predict(pca1, newdata = exprs(xx))
+        ans
+    })
+    for (i in seq_along(x)) {
+        xx <- object@x[[i]]
+        exprs(xx) <- preds[[i]]
+        sampleNames(xx) <-
+            paste0("PC", 1:ncol(xx))
+        xx <- MSnbase:::logging(xx, "remapped (PCA)")
+        object@x[[i]] <- xx
+    }
+    exprs(object@x[[ref]]) <- pca1$x
+    object@log[["remap"]] <- list(type = "PCA", prcomp = pca1, ref = ref)
+    if (validObject(object))
+        return(object)
+}
+
+
+.pdist <- function(x, y, distfun) {
+    e1 <- exprs(x)
+    e2 <- exprs(y)
+    sapply(seq_len(nrow(x)),
+           function(i) distfun(rbind(e1[i, ], e2[i, ])))
+}
+
+pdist <- function(object, distfun = dist, simplify = TRUE) {
+    stopifnot(inherits(object, "MSnSetList"))
+    ## keeping common feature names
+    suppressMessages(object <- commonFeatureNames(object))
+    if (!.sameNbCol(object))
+        stop("All MSnSets must have the same number of columns.")
+    if (is.null(names(object)))
+        names(object) <- seq_len(length(object))
+    pw <- combn(length(object), 2)
+    ans <- matrix(NA, ncol = ncol(pw),
+                  nrow = nrow(object[[1]]))
+    colnames(ans) <- apply(pw, 2,
+                           function(ii) paste(names(object)[ii],
+                                              collapse = "_"))
+    rownames(ans) <- featureNames(object[[1]])
+    for (k in 1:ncol(pw)) {
+        i <- pw[1, k]
+        j <- pw[2, k]
+        ans[, k] <- .pdist(object[[i]], object[[j]], distfun)
+    }
+    if (ncol(ans) == 1 & simplify)
+        ans <- ans[, 1]
+    ans
+}

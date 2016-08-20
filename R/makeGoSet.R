1f65524ae99f3352de9274bd5443427f815179b8
Laurent Gatto <l.gatto@dnavision.be>
2016-08-20 19:07:38 +0000

    Merge branch 'master' into devel
    * master:
      update news
      send biomart GO queries in chunks of 1000: closes #85
      new gh devel version
    
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRoloc@120309 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeGoSet.R
+++ R/makeGoSet.R
@@ -2,18 +2,25 @@
 ##' for a set of feature names. 
 ##'
 ##' @title Retrieve GO terms for feature names
-##' @param id An \code{character} with feature names to be pulled
-##' from biomart. If and \code{MSnSet} is provided, then
-##' \code{featureNames(id)} is used.
+##' @param id An \code{character} with feature names to be pulled from
+##'     biomart. If and \code{MSnSet} is provided, then
+##'     \code{featureNames(id)} is used.
 ##' @param namespace The GO namespace. One of
-##' \code{biological_process}, \code{cellular_component} (default)
-##' or \code{molecular_function}.
+##'     \code{biological_process}, \code{cellular_component} (default)
+##'     or \code{molecular_function}.
 ##' @param evidence The GO evidence code. See
-##' \code{showGOEvidenceCodes} for details. If \code{NULL} (default),
-##' no filtering based on the evidence code is performed.
-##' @param params An instance of class \code{"\linkS4class{AnnotationParams}"}.
+##'     \code{showGOEvidenceCodes} for details. If \code{NULL}
+##'     (default), no filtering based on the evidence code is
+##'     performed.
+##' @param params An instance of class
+##'     \code{"\linkS4class{AnnotationParams}"}.
 ##' @param verbose A \code{logical} defining verbosity of the
-##' function. Default is \code{FALSE}.
+##'     function. Default is \code{FALSE}.
+##' @param nmax As described in
+##'     \url{https://support.bioconductor.org/p/86358/}, the Biomart
+##'     result can be unreliable for large queries. This argument
+##'     splits the input in chunks of length \code{nmax} (default is
+##'     1000). If set to \code{NULL}, the query is performed in full.
 ##' @return A \code{data.frame} with relevant GO terms.
 ##' @author Laurent Gatto
 ##' @examples
@@ -27,7 +34,8 @@ getGOFromFeatures <- function(id,
                               namespace = "cellular_component",
                               evidence = NULL,
                               params = NULL,
-                              verbose = FALSE) {
+                              verbose = FALSE,
+                              nmax = 1000) {
     if (inherits(id, "MSnSet"))
         id <- featureNames(id)
     namespace <- tolower(namespace)
@@ -36,21 +44,38 @@ getGOFromFeatures <- function(id,
     namespace <- match.arg(namespace,
                            getGONamespace(),
                            several.ok = TRUE)
-    if (is.null(params)) 
+    if (is.null(params))
         params <- getAnnotationParams()
-    
     attrs <- chooseAttributesX(params)
-    ans <- getBM(attributes=c(params@filter, attrs, "go_linkage_type"),
-                 filters = params@filter, values = id,
-                 mart = params@mart, verbose = verbose)
+    if (is.null(nmax)) {
+        ans <- getBM(attributes = c(params@filter, attrs, "go_linkage_type"),
+                     filters = params@filter, values = id,
+                     mart = params@mart, verbose = verbose)
+    } else {
+        ## split filter values of there are too many
+        ## see https://support.bioconductor.org/p/86358/
+        from <- seq(1, length(id), by = nmax)
+        to <- c(from[-1] - 1, length(id))
+        ans <- vector("list", length = length(from))
+        for (k in seq_along(from))  {
+            .id <- id[from[k]:to[k]]
+            ans[[k]] <- getBM(attributes = c(params@filter, attrs, "go_linkage_type"),
+                              filters = params@filter, values = .id,
+                              mart = params@mart, verbose = verbose)
+        }
+        ans <- do.call(rbind, ans)
+    }
     sel <- ans[, attrs[2]] %in% namespace
     if (!is.null(evidence)) {
         evidence <- toupper(evidence)
-        evidence <- match.arg(evidence, getGOEvidenceCodes(), several.ok=TRUE)
+        evidence <- match.arg(evidence, getGOEvidenceCodes(),
+                              several.ok = TRUE)
         if ("EXP" %in% evidence)
-            evidence <- unique(c(evidence,"EXP", "IDA", "IPI", "IMP", "IGI", "IEP"))
+            evidence <- unique(c(evidence, "EXP", "IDA", "IPI", "IMP",
+                                 "IGI", "IEP"))
         if ("ISS" %in% evidence)
-            evidence <- unique(c(evidence,"ISO", "ISA", "ISM", "IGC", "IBA", "IBD", "IKR", "IRD", "RCA"))
+            evidence <- unique(c(evidence, "ISO", "ISA", "ISM", "IGC",
+                                 "IBA", "IBD", "IKR", "IRD", "RCA"))
         sel <- sel & ans$go_linkage_type %in% evidence
     }
     return(ans[sel,])
@@ -84,41 +109,41 @@ getGOFromFeatures <- function(id,
 makeGoSet <- function(object, params,
                       namespace = "cellular_component",
                       evidence = NULL) {
-  namespace <- match.arg(namespace,
-                         getGONamespace(),
-                         several.ok = TRUE)
-  if (inherits(object, "MSnSet")) {
-      fn <- featureNames(object)
-  } else if (is.character(object)) {
-      fn <- object
-  } else {
-      stop("object must be an MSnSet or a character")
-  }
-  if (missing(evidence)) evidence <- NULL
-  if (missing(params))
-      params <- getAnnotationParams()
-  if (is.null(params))
-      stop("Please set your annotation parameters. See ?AnnotationParams for details.")
-  godf <- getGOFromFeatures(fn,
-                            params = params,
-                            namespace = namespace,
-                            evidence = evidence)
-  attrs <- chooseAttributesX(params)
-  l <- lapply(fn,
-              function(x) godf[godf[, params@filter] == x, attrs[1]]) 
-  allgo <- unique(unlist(l))
-  gomat <- matrix(0, length(fn), length(allgo))
-  rownames(gomat) <- fn
-  colnames(gomat) <- allgo
-  for (i in 1:nrow(gomat)) 
-      gomat[i, l[[i]]] <- 1
-  goset <- new("MSnSet",
-               exprs = gomat)
-  if (inherits(object, "MSnSet"))
-      fData(goset) <- fData(object)
-  msg <- paste0("Constructed GO set using ", namespace, " namespace")
-  goset <- MSnbase:::logging(goset, msg)
-  if (validObject(goset))
-    return(goset)  
+    namespace <- match.arg(namespace,
+                           getGONamespace(),
+                           several.ok = TRUE)
+    if (inherits(object, "MSnSet")) {
+        fn <- featureNames(object)
+    } else if (is.character(object)) {
+        fn <- object
+    } else {
+        stop("object must be an MSnSet or a character")
+    }
+    if (missing(evidence)) evidence <- NULL
+    if (missing(params))
+        params <- getAnnotationParams()
+    if (is.null(params))
+        stop("Please set your annotation parameters. See ?AnnotationParams for details.")
+    godf <- getGOFromFeatures(fn,
+                              params = params,
+                              namespace = namespace,
+                              evidence = evidence)
+    attrs <- chooseAttributesX(params)
+    l <- lapply(fn,
+                function(x) godf[godf[, params@filter] == x, attrs[1]]) 
+    allgo <- unique(unlist(l))
+    gomat <- matrix(0, length(fn), length(allgo))
+    rownames(gomat) <- fn
+    colnames(gomat) <- allgo
+    for (i in 1:nrow(gomat)) 
+        gomat[i, l[[i]]] <- 1
+    goset <- new("MSnSet",
+                 exprs = gomat)
+    if (inherits(object, "MSnSet"))
+        fData(goset) <- fData(object)
+    msg <- paste0("Constructed GO set using ", namespace, " namespace")
+    goset <- MSnbase:::logging(goset, msg)
+    if (validObject(goset))
+        return(goset)  
 }
 

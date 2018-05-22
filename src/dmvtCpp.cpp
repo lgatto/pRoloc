17721649677d93dd0c75e4b8aa8a1ed8082c084a
Laurent <lg390@cam.ac.uk>
2018-05-22 15:57:40 +0100

    trying to export dmvtCpp

--- src/dmvtCpp.cpp
+++ src/dmvtCpp.cpp
@@ -2,8 +2,8 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 
 using namespace Rcpp ;
-//'  Copyright (C) 2014 Matteo Fasiolo  matteo.fasiolo@gmail.com
-//'This program is free software; you can redistribute it and/or
+//' Copyright (C) 2014 Matteo Fasiolo  matteo.fasiolo@gmail.com
+//' This program is free software; you can redistribute it and/or
 //' modify it under the terms of the GNU General Public License
 //' as published by the Free Software Foundation; either version 2
 //' of the License, or (at your option) any later version.
@@ -137,7 +137,7 @@ return out;
 
 
 
-//[[Rcpp::export]]
+// [[Rcpp::export]]
 SEXP dmvtCpp( arma::mat X_,  
               arma::vec mu_,  
               arma::mat sigma_, 

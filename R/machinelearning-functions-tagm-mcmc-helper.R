b782b6cb4b3795c44a2f534a16689311052d528f
Oliver M. Crook <ococrook@gmail.com>
2018-10-31 15:59:33 +0000

    Fix bug in burnin function

--- R/machinelearning-functions-tagm-mcmc-helper.R
+++ R/machinelearning-functions-tagm-mcmc-helper.R
@@ -1,47 +1,31 @@
-##' Helper function to get the number of outlier at each MCMC
-##' iteration.
-##'
-##' @title Number of outlier at each iteration of MCMC
-##' @param x Object of class `MCMCParams`
-##' @return A `list` of length `length(x)`.
-##' @md
+
+# Helper function to get number of outlier at each iteration of MCMC
 mcmc_get_outliers <- function(x) {
     stopifnot(inherits(x, "MCMCParams"))
     lapply(x@chains@chains, function(mc) coda::mcmc(colSums(1 - mc@Outlier)))
 }
 
+# Helper function to get mean component allocation at each iteration of MCMC
 
-
-##' Helper function to get mean component allocation at each MCMC
-##' iteration.
-##' @title Mean component allocation at each MCMC iteration
-##' @param x Object of class `MCMCParams`
-##' @return A `list` of length `length(x)`.
 mcmc_get_meanComponent <- function(x) {
   stopifnot(inherits(x, "MCMCParams"))
   lapply(x@chains@chains, function(mc) coda::mcmc(colMeans(mc@Component)))
 }
 
+# Helper function to get mean probability of belonging to outlier at each iteration
 
-##' Helper function to get mean probability of belonging to outlier at
-##' each iteration.
-##' @title Mean outlier probability
-##' @param x Object of class `MCMCParams`
-##' @return A `list` of length `length(x)`.
-##' @author Laurent Gatto
 mcmc_get_meanoutliersProb <- function(x) {
   stopifnot(inherits(x, "MCMCParams"))
   lapply(x@chains@chains, function(mc) coda::mcmc(colMeans(mc@OutlierProb[, ,2])))
 }
 
+# Wrapper for the geweke diagnostics from coda package also return p-values
 
-##' Wrapper for the geweke diagnostics from the coda package also
-##' return p-values.
 geweke_test <- function(x) {
   res <- matrix(NA, nrow = 2, ncol = length(x))
   gwk <- sapply(x, coda::geweke.diag, simplify = TRUE)
   res[1, ] <- unlist(gwk[1, ])
-  res[2, ] <- pnorm(abs(unlist(gwk[1, ])), lower.tail = FALSE) * 2
+  res[2, ] <- pnorm(abs(unlist(gwk[1, ])), lower.tail=FALSE) * 2
   colnames(res) <- paste0("chain ", seq.int(x))
   rownames(res) <- c("z.value", "p.value")
   return(res)
@@ -51,6 +35,7 @@ geweke_test <- function(x) {
 
 mcmc_pool_chains <- function(param) {
   stopifnot(inherits(param, "MCMCParams"))
+  
   param1 <- pRoloc:::chains(param)[[1]]
 
   n <- param1@n
@@ -58,36 +43,36 @@ mcmc_pool_chains <- function(param) {
   KPool <- param1@K  # number of components unchanged
   NPool <- param1@N # number of proteins doesn't change
   numChains <- length(param)
-
+  
   pooled.Component <- matrix(0, nrow = NPool, ncol = nPool)
   pooled.ComponentProb <- array(0, c(NPool, nPool, KPool ))
   pooled.Outlier <- matrix(0, nrow = NPool, ncol = nPool)
   pooled.OutlierProb <- array(0, c(NPool, nPool, 2 ))
-
+  
   rownames(pooled.Component) <- rownames(param1@Component)
   rownames(pooled.ComponentProb) <- rownames(param1@ComponentProb)
   rownames(pooled.Outlier) <- rownames(param1@Outlier)
   rownames(pooled.OutlierProb) <- rownames(param1@OutlierProb)
   dimnames(pooled.ComponentProb)[[3]] <- dimnames(param1@ComponentProb)[[3]]
-
-
+  
+  
   # Calculate basic quantities
   for (j in seq_len(numChains)) {
-
+    
     mc <- pRoloc:::chains(param)[[j]]
     ## Pool chains
     pooled.Component[, n * (j - 1) + seq.int(n)] <- mc@Component
     pooled.ComponentProb[, n * (j - 1) + seq.int(n), ] <- mc@ComponentProb
     pooled.Outlier[, n * (j - 1)+ seq.int(n)] <- mc@Outlier
     pooled.OutlierProb[, n * (j - 1) + seq.int(n), ] <- mc@OutlierProb
-
+    
   }
-
+  
   mk.list <- lapply(param@chains@chains,function(x) x@ComponentParam@mk)
   lambdak.list <- lapply(param@chains@chains,function(x) x@ComponentParam@lambdak)
   nuk.list <- lapply(param@chains@chains,function(x) x@ComponentParam@nuk)
   sk.list <- lapply(param@chains@chains,function(x) x@ComponentParam@sk)
-
+  
   ## save Component parameters
   .ComponentParam <- pRoloc:::.ComponentParam(K = KPool, D = param1@ComponentParam@D,
                                      mk = Reduce("+", mk.list) / length(mk.list),
@@ -99,7 +84,7 @@ mcmc_pool_chains <- function(param) {
   .ComponentProb <- pooled.ComponentProb
   .Outlier <- pooled.Outlier
   .OutlierProb <- pooled.OutlierProb
-
+  
   ## make MCMCChain object
   .MCMCChain <- pRoloc:::.MCMCChain(n = nPool,
                            K = KPool,
@@ -109,16 +94,16 @@ mcmc_pool_chains <- function(param) {
                            Outlier = .Outlier,
                            OutlierProb = .OutlierProb,
                            ComponentParam = .ComponentParam)
-
+  
   ## Make MCMCChains with single object
   .MCMCChains <- pRoloc:::.MCMCChains(chains = list(.MCMCChain))
-
+  
   ## Make MCMCParams object
   pRoloc:::.MCMCParams(method = "TAGM.MCMC",
                        chains = .MCMCChains,
                        priors = param@priors,
                        summary = pRoloc:::.MCMCSummary())
-
+  
 }
 
 # Helper function to burn n iterations from the front of the chains
@@ -134,12 +119,12 @@ mcmc_burn_chains <- function(x, n) {
             ## Subset MCMC iterations
             retain <- seq.int(n + 1, chain@n) ## samples to retain
             ## Check correct number of iterations
-            stopifnot(ncol(chain@Component[, retain]) == n)
+            stopifnot(ncol(chain@Component[, retain]) == (chain@n - n))
             .Component <- chain@Component[, retain]
             .ComponentProb <- chain@ComponentProb[, retain, ]
             .Outlier <- chain@Outlier[, retain]
             .OutlierProb <- chain@OutlierProb[, retain, ]
-            pRoloc:::.MCMCChain(n = as.integer(n),
+            pRoloc:::.MCMCChain(n = as.integer(chain@n - n),
                                 K = K,
                                 N = N,
                                 Component = .Component,
@@ -152,7 +137,7 @@ mcmc_burn_chains <- function(x, n) {
     mcmc_chainlist <- pRoloc:::.MCMCChains(chains = chainlist)
     pRoloc:::.MCMCParams(method = "TAGM.MCMC",
                          chains = mcmc_chainlist,
-                         priors = param@priors,
+                         priors = x@priors,
                          summary = pRoloc:::.MCMCSummary())
 }
 
@@ -184,7 +169,7 @@ mcmc_thin_chains <- function(x, freq) {
                           OutlierProb = .OutlierProb,
                           ComponentParam = .ComponentParam)
     })
-
+  
   mcmc_chainlist <- pRoloc:::.MCMCChains(chains = chainlist)
   pRoloc:::.MCMCParams(method = "TAGM.MCMC",
                        chains = mcmc_chainlist,
@@ -192,16 +177,15 @@ mcmc_thin_chains <- function(x, freq) {
                        summary = pRoloc:::.MCMCSummary())
 }
 
-## Plotting method for violins
+# Plotting method for violins
+
 setMethod("plot", c("MCMCParams", "character"),
           function(x, y, ...) {
               mcmc_plot_probs(x, y, n = 1)
           })
+# Plotting function for violins using ggplot2.
 
-## Plotting function for violins using ggplot2.
 mcmc_plot_probs <- function(param, fname, n = 1) {
-    ## no visible global function definition
-    Organelle <- Probability <- NULL
     stopifnot(inherits(param, "MCMCParams"))
     stopifnot(length(fname) == 1)
     chain <- pRoloc:::chains(param)[[n]]
@@ -216,10 +200,10 @@ mcmc_plot_probs <- function(param, fname, n = 1) {
                   aes(Organelle, Probability,
                       width = (Probability))) +
         geom_violin(aes(fill = Organelle), scale = "width", bw = 0.05)
-    gg2 <- gg2 + theme_bw() +
+    gg2 <- gg2 + theme_bw() + 
         scale_fill_manual(values = pRoloc::getStockcol()[seq_len(nrow(dfr))]) +
         theme(axis.text.x = element_text(angle = 90, hjust = 1),
-              axis.title.x = element_blank())
+              axis.title.x = element_blank()) 
     gg2 <- gg2 +
         ylab("Membership Probability") +
         ggtitle(paste0("Distribution of Subcellular Membership for Protein ", fname ))

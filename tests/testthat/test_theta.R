c512592c619f1a5b8e32faab167b9174dd0bc46a
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-03-15 09:21:56 +0100

    add test back

--- tests/testthat/test_theta.R
+++ tests/testthat/test_theta.R
@@ -1,130 +1,130 @@
 context("thetaFunctions")
 
-## test_that("theta matrices passed to knntl are the same as those output and stored in hyperparameters", {
-##   library("pRolocdata")
-##   data(andy2011)
-##   data(andy2011goCC)
+test_that("theta matrices passed to knntl are the same as those output and stored in hyperparameters", {
+  library("pRolocdata")
+  data(andy2011)
+  data(andy2011goCC)
 
-##   ## Do we get the same results with and without specifying
-##   ## method = "Breckels"
-##   seed <- sample(.Machine$integer.max, 1)
-##   tl1 <- knntlOptimisation(andy2011, andy2011goCC,
-##                            fcol = "markers.orig",
-##                            times = 1,
-##                            by = 1, k = c(3, 3),
-##                            BPPARAM = SerialParam(),
-##                            method = "Breckels",
-##                            seed = seed)
-##   tl2 <- knntlOptimisation(andy2011, andy2011goCC,
-##                            fcol = "markers.orig",
-##                            times = 1,
-##                            by = 1, k = c(3, 3),
-##                            BPPARAM = SerialParam(),
-##                            seed = seed)
-##   expect_equal(tl1, tl2)
+  ## Do we get the same results with and without specifying
+  ## method = "Breckels"
+  seed <- sample(.Machine$integer.max, 1)
+  tl1 <- knntlOptimisation(andy2011, andy2011goCC,
+                           fcol = "markers.orig",
+                           times = 1,
+                           by = 1, k = c(3, 3),
+                           BPPARAM = SerialParam(),
+                           method = "Breckels",
+                           seed = seed)
+  tl2 <- knntlOptimisation(andy2011, andy2011goCC,
+                           fcol = "markers.orig",
+                           times = 1,
+                           by = 1, k = c(3, 3),
+                           BPPARAM = SerialParam(),
+                           seed = seed)
+  expect_equal(tl1, tl2)
 
-##   expect_is(combineThetaRegRes(list(tl1, tl2)), "ThetaRegRes")
+  expect_is(combineThetaRegRes(list(tl1, tl2)), "ThetaRegRes")
 
-##   par <- getParams(tl1)
-##   res1 <- knntlClassification(andy2011, andy2011goCC,
-##                               fcol = "markers.orig",
-##                               bestTheta = par, k = c(3,3),
-##                               seed = seed)
-##   res2 <- knntlClassification(andy2011, andy2011goCC,
-##                               fcol = "markers.orig",
-##                               bestTheta = par, k = c(3,3),
-##                               seed = seed)
-##   expect_equal(fData(res1)$knntl.scores, fData(res2)$knntl.scores)
-##   expect_equal(fData(res1)$knntl, fData(res2)$knntl)
+  par <- getParams(tl1)
+  res1 <- knntlClassification(andy2011, andy2011goCC,
+                              fcol = "markers.orig",
+                              bestTheta = par, k = c(3,3),
+                              seed = seed)
+  res2 <- knntlClassification(andy2011, andy2011goCC,
+                              fcol = "markers.orig",
+                              bestTheta = par, k = c(3,3),
+                              seed = seed)
+  expect_equal(fData(res1)$knntl.scores, fData(res2)$knntl.scores)
+  expect_equal(fData(res1)$knntl, fData(res2)$knntl)
 
-##   ## Test whether passing weights using the thetas function
-##   ## or calculating internally produces the same results
-##   n <- length(getMarkerClasses(andy2011,fcol = "markers.orig"))
-##   weights <- thetas(nclass = n, by = 1, verbose = FALSE)
+  ## Test whether passing weights using the thetas function
+  ## or calculating internally produces the same results
+  n <- length(getMarkerClasses(andy2011,fcol = "markers.orig"))
+  weights <- thetas(nclass = n, by = 1, verbose = FALSE)
 
-##   tl3 <- knntlOptimisation(andy2011, andy2011goCC,
-##                            fcol = "markers.orig",
-##                            times = 1, th = weights,
-##                            by = 1, k = c(3, 3),
-##                            BPPARAM = SerialParam(),
-##                            seed = seed)
-##   expect_equal(tl1, tl3)
+  tl3 <- knntlOptimisation(andy2011, andy2011goCC,
+                           fcol = "markers.orig",
+                           times = 1, th = weights,
+                           by = 1, k = c(3, 3),
+                           BPPARAM = SerialParam(),
+                           seed = seed)
+  expect_equal(tl1, tl3)
 
-##   ## Test whether Wu weights are calculated correctly i.e. the same weight
-##   ## per class, data-specific not class-specific
-##   wuweights <- matrix(rep(seq(0, 1, length.out = 4), n), ncol = n)
-##   tl4 <-  knntlOptimisation(andy2011, andy2011goCC,
-##                             fcol = "markers.orig",
-##                             times = 1,
-##                             length.out = 4,
-##                             k = c(3, 3),
-##                             BPPARAM = SerialParam(),
-##                             method = "Wu",
-##                             seed = seed)
-##   tl5 <-  knntlOptimisation(andy2011, andy2011goCC,
-##                             fcol = "markers.orig",
-##                             times = 1,
-##                             length.out = 4,
-##                             k = c(3, 3),
-##                             BPPARAM = SerialParam(),
-##                             th = wuweights,
-##                             seed = seed)
-##   expect_equal(tl4, tl5)
+  ## Test whether Wu weights are calculated correctly i.e. the same weight
+  ## per class, data-specific not class-specific
+  wuweights <- matrix(rep(seq(0, 1, length.out = 4), n), ncol = n)
+  tl4 <-  knntlOptimisation(andy2011, andy2011goCC,
+                            fcol = "markers.orig",
+                            times = 1,
+                            length.out = 4,
+                            k = c(3, 3),
+                            BPPARAM = SerialParam(),
+                            method = "Wu",
+                            seed = seed)
+  tl5 <-  knntlOptimisation(andy2011, andy2011goCC,
+                            fcol = "markers.orig",
+                            times = 1,
+                            length.out = 4,
+                            k = c(3, 3),
+                            BPPARAM = SerialParam(),
+                            th = wuweights,
+                            seed = seed)
+  expect_equal(tl4, tl5)
 
-##   ## Test if the auxiliary is a bigger (more proteins) *and* the markers
-##   ## are the same we get the same results
-##   torm <- sample(which(fData(andy2011)$markers.orig == "unknown"), 100)
-##   andysmall <- andy2011[-torm, ]
+  ## Test if the auxiliary is a bigger (more proteins) *and* the markers
+  ## are the same we get the same results
+  torm <- sample(which(fData(andy2011)$markers.orig == "unknown"), 100)
+  andysmall <- andy2011[-torm, ]
 
-##   tl1 <- knntlOptimisation(andysmall, andy2011goCC,
-##                            fcol = "markers.orig",
-##                            times = 1,
-##                            by = 1, k = c(3, 3),
-##                            BPPARAM = SerialParam(),
-##                            seed = seed)
+  tl1 <- knntlOptimisation(andysmall, andy2011goCC,
+                           fcol = "markers.orig",
+                           times = 1,
+                           by = 1, k = c(3, 3),
+                           BPPARAM = SerialParam(),
+                           seed = seed)
 
-##   tl2 <- knntlOptimisation(andy2011, andy2011goCC,
-##                            fcol = "markers.orig",
-##                            times = 1,
-##                            by = 1, k = c(3, 3),
-##                            BPPARAM = SerialParam(),
-##                            seed = seed)
+  tl2 <- knntlOptimisation(andy2011, andy2011goCC,
+                           fcol = "markers.orig",
+                           times = 1,
+                           by = 1, k = c(3, 3),
+                           BPPARAM = SerialParam(),
+                           seed = seed)
 
-##   expect_identical(tl1, tl2)
+  expect_identical(tl1, tl2)
 
-##   ## Test if the auxiliary is a bigger (more proteins) *and* the markers
-##   ## are different code doesn't break
-##   torm <- sample(which(fData(andy2011)$markers.orig != "unknown"), 100)
-##   andysmall <- andy2011[-torm, ]
-##   tl <- knntlOptimisation(andysmall, andy2011goCC,
-##                           fcol = "markers.orig",
-##                           times = 1,
-##                           by = 1, k = c(3, 3),
-##                           BPPARAM = SerialParam(),
-##                           seed = seed)
-##   expect_true(validObject(tl))
+  ## Test if the auxiliary is a bigger (more proteins) *and* the markers
+  ## are different code doesn't break
+  torm <- sample(which(fData(andy2011)$markers.orig != "unknown"), 100)
+  andysmall <- andy2011[-torm, ]
+  tl <- knntlOptimisation(andysmall, andy2011goCC,
+                          fcol = "markers.orig",
+                          times = 1,
+                          by = 1, k = c(3, 3),
+                          BPPARAM = SerialParam(),
+                          seed = seed)
+  expect_true(validObject(tl))
 
-## #   ## check logging and no-logging results in the same results
-## #   seed <- sample(.Machine$integer.max, 1)
-## #   tl1 <- knntlOptimisation(andy2011, andy2011goCC,
-## #                            fcol = "markers.orig",
-## #                            times = 1,
-## #                            by = 1, k = c(3, 3),
-## #                            BPPARAM = SerialParam(),
-## #                            seed = seed)
-## #
-## #   tl2 <- knntlOptimisation(andy2011, andy2011goCC,
-## #                            fcol = "markers.orig",
-## #                            times = 1,
-## #                            by = 1, k = c(3, 3),
-## #                            BPPARAM = SerialParam(),
-## #                            seed = seed,
-## #                            log = TRUE)
-## #
-## #   expect_identical(tl1@results, tl2@results)
-## #   expect_identical(tl1@hyperparameters, tl2@hyperparameters)
+#   ## check logging and no-logging results in the same results
+#   seed <- sample(.Machine$integer.max, 1)
+#   tl1 <- knntlOptimisation(andy2011, andy2011goCC,
+#                            fcol = "markers.orig",
+#                            times = 1,
+#                            by = 1, k = c(3, 3),
+#                            BPPARAM = SerialParam(),
+#                            seed = seed)
+#
+#   tl2 <- knntlOptimisation(andy2011, andy2011goCC,
+#                            fcol = "markers.orig",
+#                            times = 1,
+#                            by = 1, k = c(3, 3),
+#                            BPPARAM = SerialParam(),
+#                            seed = seed,
+#                            log = TRUE)
+#
+#   expect_identical(tl1@results, tl2@results)
+#   expect_identical(tl1@hyperparameters, tl2@hyperparameters)
 
-## })
+})
 
 
 

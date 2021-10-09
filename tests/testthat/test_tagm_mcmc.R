1a3ec7b4233f3e9da532392e01591cae957c4047
Laurent Gatto <laurent.gatto@uclouvain.be>
2021-10-09 08:41:42 +0200

    fix typo

--- tests/testthat/test_tagm_mcmc.R
+++ tests/testthat/test_tagm_mcmc.R
@@ -18,7 +18,7 @@ test_that("TAGM MCMC consistency", {
   set.seed(1)
   mcmc2 <- tagmMcmcTrain(object = d2, fcol = "xx",
                          numIter = .numIter, burnin = 0, thin = 1, numChains = 2,
-                         BPPARAM = SerialParam(RNGsed = 1))
+                         BPPARAM = SerialParam(RNGseed = 1))
   expect_equal(mcmc1, mcmc2)
   mcmc1 <- tagmMcmcProcess(mcmc1)
   mcmc2 <- tagmMcmcProcess(mcmc2)

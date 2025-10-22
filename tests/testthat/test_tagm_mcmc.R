04d3b858b9c35ac03b4bef7098356d66e9351b6b
Lisa Breckels <lms79@cam.ac.uk>
2025-10-22 22:46:31 +0100

    fix tagm-mcmc unit tests

--- tests/testthat/test_tagm_mcmc.R
+++ tests/testthat/test_tagm_mcmc.R
@@ -33,11 +33,11 @@ test_that("TAGM MCMC V2 consistency", {
   set.seed(1)
   mcmc1 <- tagmMcmcTrain(object = d1, fcol = "markers",
                          numIter = .numIter,burnin = 0, thin = 1, numChains = 2,
-                         BPPARAM = SerialParam(RNGseed = 1), V2 = TRUE)
+                         BPPARAM = SerialParam(RNGseed = 1), version = 2)
   set.seed(1)
   mcmc2 <- tagmMcmcTrain(object = d2, fcol = "xx",
                          numIter = .numIter, burnin = 0, thin = 1, numChains = 2,
-                         BPPARAM = SerialParam(RNGseed = 1), V2 = TRUE)
+                         BPPARAM = SerialParam(RNGseed = 1), version = 2)
   expect_equal(mcmc1, mcmc2)
   mcmc1 <- tagmMcmcProcess(mcmc1)
   mcmc2 <- tagmMcmcProcess(mcmc2)
